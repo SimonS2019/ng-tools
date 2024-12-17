6e8ea0cb2985da581278070178f239b4ef4e4b16
SimonS2019 <shentiexiong@gmail.com>
2024-12-17 16:07:28 +1300

    add Bootstrap and ngx-bootstrap, update global styles and app title

--- ng-tools/src/app/app.module.ts
+++ ng-tools/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot() // Add this line
   ],
   providers: [],
   bootstrap: [AppComponent]
