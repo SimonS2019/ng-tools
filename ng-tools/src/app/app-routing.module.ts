049112ba7a75a635efbf7157bb6d4dae418397e5
SimonS2019 <shentiexiong@gmail.com>
2024-12-18 14:41:15 +1300

    add routing for txt-reader component and update navigation link

--- ng-tools/src/app/app-routing.module.ts
+++ ng-tools/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TxtReaderComponent } from './txt-reader/txt-reader/txt-reader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'txt-reader', component: TxtReaderComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-tools/src/app/txt-reader/txt-reader-routing.module.ts
+++ ng-tools/src/app/txt-reader/txt-reader-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TxtReaderComponent } from './txt-reader/txt-reader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TxtReaderComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
